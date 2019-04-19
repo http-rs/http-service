7844f703fdce97cba7b7424c652beb78ad4eed8e
Bastian Gruber <gruberbastian@me.com>
2019-04-19 07:32:10 +0200

    Formatting

--- examples/simple_response.rs
+++ examples/simple_response.rs
@@ -1,8 +1,6 @@
 #![feature(futures_api, async_await, await_macro, existential_type)]
 
-use futures::{
-    future::{self, FutureObj},
-};
+use futures::future::{self, FutureObj};
 use http_service::{HttpService, Response};
 
 struct Server {
@@ -11,9 +9,7 @@ struct Server {
 
 impl Server {
     fn create(message: Vec<u8>) -> Server {
-        Server {
-            message,
-        }
+        Server { message }
     }
 }
 
@@ -21,17 +17,15 @@ impl HttpService for Server {
     type Connection = ();
     type ConnectionFuture = future::Ready<Result<(), std::io::Error>>;
     type Fut = FutureObj<'static, Result<http_service::Response, std::io::Error>>;
-    
+
     fn connect(&self) -> Self::ConnectionFuture {
         future::ok(())
     }
 
     fn respond(&self, _conn: &mut (), _req: http_service::Request) -> Self::Fut {
         let message = self.message.clone();
-        FutureObj::new(Box::new(
-            async move {
-                Ok(Response::new(http_service::Body::from(message)))
-            }
-        ))
+        FutureObj::new(Box::new(async move {
+            Ok(Response::new(http_service::Body::from(message)))
+        }))
     }
 }
