2be0f2b1a4c3f0e8a7d93f55bae919f05941e7e4
Wim Looman <wim@nemo157.com>
2019-06-24 22:50:34 +0200

    Add builder to allow configuring the spawner used and allow disabling hyper runtime feature

--- http-service-hyper/src/lib.rs
+++ http-service-hyper/src/lib.rs
@@ -6,13 +6,24 @@
 #![cfg_attr(test, deny(warnings))]
 #![feature(async_await)]
 
+#[cfg(feature = "runtime")]
+use futures::compat::Future01CompatExt;
 use futures::{
-    compat::{Compat, Compat01As03, Future01CompatExt},
+    compat::{Compat, Compat01As03},
     future::BoxFuture,
     prelude::*,
+    stream,
+    task::Spawn,
 };
 use http_service::{Body, HttpService};
-use std::{net::SocketAddr, sync::Arc};
+use hyper::server::{Builder as HyperBuilder, Server as HyperServer};
+#[cfg(feature = "runtime")]
+use std::net::SocketAddr;
+use std::{
+    pin::Pin,
+    sync::Arc,
+    task::{self, Poll},
+};
 
 // Wrapper type to allow us to provide a blanket `MakeService` impl
 struct WrapHttpService<H> {
@@ -80,8 +91,120 @@ where
     }
 }
 
+/// A listening HTTP server that accepts connections in both HTTP1 and HTTP2 by default.
+///
+/// [`Server`] is a [`Future`] mapping a bound listener with a set of service handlers. It is built
+/// using the [`Builder`], and the future completes when the server has been shutdown. It should be
+/// run by an executor.
+#[allow(clippy::type_complexity)] // single-use type with many compat layers
+pub struct Server<I: TryStream, S, Sp> {
+    inner: Compat01As03<
+        HyperServer<
+            Compat<stream::MapOk<I, fn(I::Ok) -> Compat<I::Ok>>>,
+            WrapHttpService<S>,
+            Compat<Sp>,
+        >,
+    >,
+}
+
+impl<I: TryStream, S, Sp> std::fmt::Debug for Server<I, S, Sp> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Server").finish()
+    }
+}
+
+/// A builder for a [`Server`].
+#[allow(clippy::type_complexity)] // single-use type with many compat layers
+pub struct Builder<I: TryStream, Sp> {
+    inner: HyperBuilder<Compat<stream::MapOk<I, fn(I::Ok) -> Compat<I::Ok>>>, Compat<Sp>>,
+}
+
+impl<I: TryStream, Sp> std::fmt::Debug for Builder<I, Sp> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Builder").finish()
+    }
+}
+
+impl<I: TryStream> Server<I, (), ()> {
+    /// Starts a [`Builder`] with the provided incoming stream.
+    pub fn builder(incoming: I) -> Builder<I, ()> {
+        Builder {
+            inner: HyperServer::builder(Compat::new(incoming.map_ok(Compat::new as _)))
+                .executor(Compat::new(())),
+        }
+    }
+}
+
+impl<I: TryStream, Sp> Builder<I, Sp> {
+    /// Sets the [`Spawn`] to deal with starting connection tasks.
+    pub fn with_spawner<Sp2>(self, new_spawner: Sp2) -> Builder<I, Sp2> {
+        Builder {
+            inner: self.inner.executor(Compat::new(new_spawner)),
+        }
+    }
+
+    /// Consume this [`Builder`], creating a [`Server`].
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use http_service::{Response, Body};
+    /// use http_service_hyper::Server;
+    /// use romio::TcpListener;
+    ///
+    /// // Construct an executor to run our tasks on
+    /// let mut pool = futures::executor::ThreadPool::new()?;
+    ///
+    /// // And an HttpService to handle each connection...
+    /// let service = |req| {
+    ///     futures::future::ok::<_, ()>(Response::new(Body::from("Hello World")))
+    /// };
+    ///
+    /// // Then bind, configure the spawner to our pool, and serve...
+    /// let addr = "127.0.0.1:3000".parse()?;
+    /// let mut listener = TcpListener::bind(&addr)?;
+    /// let server = Server::builder(listener.incoming())
+    ///     .with_spawner(pool.clone())
+    ///     .serve(service);
+    ///
+    /// // Finally, spawn `server` onto our executor...
+    /// pool.run(server)?;
+    /// # Ok::<(), Box<dyn std::error::Error>>(())
+    pub fn serve<S: HttpService>(self, service: S) -> Server<I, S, Sp>
+    where
+        I: TryStream + Unpin,
+        I::Ok: AsyncRead + AsyncWrite + Send + Unpin + 'static,
+        I::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+        Sp: Clone + Send + 'static,
+        for<'a> &'a Sp: Spawn,
+    {
+        Server {
+            inner: Compat01As03::new(self.inner.serve(WrapHttpService {
+                service: Arc::new(service),
+            })),
+        }
+    }
+}
+
+impl<I, S, Sp> Future for Server<I, S, Sp>
+where
+    I: TryStream + Unpin,
+    I::Ok: AsyncRead + AsyncWrite + Send + Unpin + 'static,
+    I::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
+    S: HttpService,
+    Sp: Clone + Send + 'static,
+    for<'a> &'a Sp: Spawn,
+{
+    type Output = hyper::Result<()>;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut task::Context<'_>) -> Poll<hyper::Result<()>> {
+        self.inner.poll_unpin(cx)
+    }
+}
+
 /// Serve the given `HttpService` at the given address, using `hyper` as backend, and return a
 /// `Future` that can be `await`ed on.
+#[cfg(feature = "runtime")]
 pub fn serve<S: HttpService>(
     s: S,
     addr: SocketAddr,
@@ -94,6 +217,7 @@ pub fn serve<S: HttpService>(
 
 /// Run the given `HttpService` at the given address on the default runtime, using `hyper` as
 /// backend.
+#[cfg(feature = "runtime")]
 pub fn run<S: HttpService>(s: S, addr: SocketAddr) {
     let server = serve(s, addr).map(|_| Result::<_, ()>::Ok(())).compat();
     hyper::rt::run(server);
